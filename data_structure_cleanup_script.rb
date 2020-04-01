408e09d75f1584e23e0520cd407723512170284a
Gregory Havenga <gregory.havenga@gmail.com>
2020-04-01 21:22:47 +0200

    Touchups on the data cleanup script

--- data_structure_cleanup_script.rb
+++ data_structure_cleanup_script.rb
@@ -4,24 +4,24 @@ require 'yaml'
 cleaned_data = YAML.load_file('south_africa_provincial_stats.yml').map do |day|
   obj = {
     date: day[:date],
-    total: day[:total].to_i,
-    unnallocated: day[:unnallocated].to_i,
-    deaths: day[:deaths].to_i,
+    total: day[:total],
+    unnallocated: day[:unnallocated],
+    deaths: day[:deaths],
     sources: day[:sources],
-    tests_done: day[:tests_done].to_i,
-    negative_tests_results: day[:negative_tests_results].to_i,
+    tests_done: day[:tests_done],
+    negative_tests_results: day[:negative_tests_results],
     gender: {
-      male: day.dig(:gender, :male).to_i || day[:male].to_i,
-      female: day.dig(:gender, :female).to_i || day[:female].to_i,
-      unknown: day.dig(:gender, :unknown).to_i || day[:unknown].to_i
+      male: day.dig(:gender, :male) || day[:male],
+      female: day.dig(:gender, :female) || day[:female],
+      unknown: day.dig(:gender, :unknown) || day[:unknown]
     },
-    local_transmissions: day[:local_transmissions].to_i
+    local_transmissions: day[:local_transmissions]
   }
 
   %i(eastern_cape kwazulu_natal mpumalanga western_cape northern_cape north_west free_state gauteng limpopo).each do |place|
     obj[place] = {
-      total: day[place].is_a?(Hash) ? day.dig(place, :total).to_i : day[place].to_i,
-      deaths: day[place].is_a?(Hash) ? day.dig(place, :deaths).to_i : 0 ,
+      total: day[place].is_a?(Hash) ? day.dig(place, :total) : day[place],
+      deaths: day[place].is_a?(Hash) ? day.dig(place, :deaths) : 0 ,
     }
   end
   obj
